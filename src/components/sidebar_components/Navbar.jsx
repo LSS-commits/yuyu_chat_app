207257b1c65fc3bda7b119deac3f7423bf3e0fc8
LSS <lss@jaimal.com>
2022-12-06 01:10:06 +0100

    remove picto from navbar

--- src/components/sidebar_components/Navbar.jsx
+++ src/components/sidebar_components/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import Picto from "../../assets/picto.png";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={Picto} alt="yuyu app picto"/>
+      <span className='logo'>Yuyu Chat</span>
       <div className="user">
         <img src="https://images.pexels.com/photos/14616881/pexels-photo-14616881.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="user avatar"/>
         <span>Username</span>
-        <button>Sign out &#128564;</button>
+        <button>Sign out</button>
       </div>
     </div>
   );
