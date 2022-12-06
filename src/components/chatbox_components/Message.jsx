630f2380ed4a65928f8340a5d3e93932163a72ce
LSS <lss@jaimal.com>
2022-12-06 10:28:08 +0100

    edit styles

--- src/components/chatbox_components/Message.jsx
+++ src/components/chatbox_components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img src="https://images.pexels.com/photos/14616881/pexels-photo-14616881.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="user avatar"/>
         {/* TODO: Add real timestamp */}
@@ -10,7 +10,7 @@ const Message = () => {
       </div>
       <div className="messageContent">
         <p>안녕!</p>
-        {/* <img src="https://images.pexels.com/photos/14616881/pexels-photo-14616881.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="user avatar"/> */}
+        <img src="https://images.pexels.com/photos/14616881/pexels-photo-14616881.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="user avatar"/>
       </div>
     </div>
   )
